0c0ea33a192627c9af535d3c2c8c360bd0d78cef
Opilane TTHK <opilane@tthk.ee>
2023-12-05 14:42:30 +0200

    As an app developer I want to get a home cinema in my app [Delivers #186567053]

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port=8080
 const swaggerUI = require('swagger-ui-express')
 const swaggerDocument= require('./docs/swagger.json');
 
+app.get('/theaters',(req,res)=>{res .send (["The poidgeon","Kunda Kino","Paladins:Champions of the Realm ","Super Mario Bros. Wonder"])})
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.listen(port, () => {
